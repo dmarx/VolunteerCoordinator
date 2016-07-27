9d6360752b4d606b7ca560bb09b044805e896b8a
David Marx <david.marx84@gmail.com>
2016-07-26 20:52:52 -0400

    Added solver script with function to build graph representation of problem setup. Need to add sinks to graph.

--- README.txt
+++ README.txt
@@ -5,6 +5,7 @@ Target feature set:
 * Assigning volunteers to duty shifts, subject to preference constraints possibly including:
     * Shift preferences
     * Reducing number of shifts on same day for multi-day events
+    * Adding preference to sequential shifts when there are multiple shifts on the same day. (I think that makes sense?)
     * Pairing people who want to work together
     * Spreading out hours across as many people as possible as uniformly as possible (i.e. sharing the load as much as possible)
 
@@ -12,3 +13,7 @@ Target feature set:
     * Gender preference
     * Pairing people who are travelling together or would prefer to be housed together
     * Host's available space
+    
+* Identifying areas of weakness, including:
+    * Identifying which roles need to be filled when an optimal duty assignment solution does not exist
+    * Identifying housing needs when an optimal housing solution does not exist
---
+++ solver.py
@@ -0,0 +1,46 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import networkx as nx
+import pulp
+from collections import Counter, defaultdict
+
+from scipy.stats import poisson # for clique problem
+from networkx.algorithms import bipartite # for clique problem
+import random # for the demo
+import gc # for memory management associated with the demo
+import time # performance evaluation
+
+# Shift assignment and housing assignment are essentially separate problems
+# Each problem should have an associated utility for identifying what weakness needs to be satisfied
+# when an optimal solution does not exist.
+
+# Let's start with the duty assignment problem. Preferecnes will be determined via survey: let's assume
+# we let people rank their preferences. Expect data to come in a CSV matrix whose rows are volunteers, 
+# columns are duty shifts (day - hour/shift pairs) and values are preference ranks, where 1 is the most 
+# desired shift, and empty cells denote blocked shifts for that volunteer.
+
+def build_shifts_graph(df):
+    """
+    Takes as input a dataframe read in from a CSV, where the columns are given by labels like "day1|shft1"
+    and the row labels give volunteer names.
+    """
+    g = nx.DiGraph()
+    for day_shift in df.columns:
+        for vol in df.index:
+            val = df[day_shift][vol]
+            if np.isnan(val):
+                continue
+            day, shift = day_shift.split('|') # Not sure exactly what to do with this. I think day should be a node attr
+            g.add_edge(vol, day, {'shift_{}'.format(shift):int(val)})
+    return g
+
+def build_problem_from_graph(g):
+    pass
+    
+if __name__ == '__main__':
+    import pandas as pd
+    
+    fname = 'demo_schedule.csv'
+    df = pd.DataFrame.from_csv(fname)
+    
